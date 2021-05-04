cc45e33a4a2db80cfe7ad1d0c75ee437d725e72e
kellenrowe <kellenrowe@gmail.com>
2021-05-04 10:50:32 -0700

    added smoke tests
    added smoke tests to Form and App files to ensure each component renders without failure
    App test mocks a button click and tests that a room with default name is entered

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
 import App from './App';
 
-test('renders learn react link', () => {
+const HOME_TAGLINE = 'Kinda like chutney but a bit more saucy';
+const HOME_BTN_TXT = 'Join Room'
+const DFLT_ROOM_NAME = 'Chat Room'
+
+test("renders App without crashing", function () {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('Home and ChatRoom Render', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+  // verify page content for expected route
+  // often you'd use a data-testid or role query, but this is also possible
+  expect(screen.getByText(HOME_TAGLINE)).toBeInTheDocument()
+
+  const leftClick = { button: 0 }
+  userEvent.click(screen.getByText(HOME_BTN_TXT), leftClick)
+
+  // check that the content changed to the new page
+  expect(screen.getByText(DFLT_ROOM_NAME)).toBeInTheDocument()
+})
---
+++ client/src/Form.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Form from './Form';
+
+it("renders without crashing", function () {
+  render(<Form />);
+});
